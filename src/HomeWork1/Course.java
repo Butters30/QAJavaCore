1e6356476b16997853eaa2e8b74fb9df28006a11
etern <eternal_smile90@bk.ru>
2021-11-20 20:05:43 +0200

    HomeWork1

---
+++ src/HomeWork1/Course.java
@@ -0,0 +1,11 @@
+package HomeWork1;
+
+public class Course {
+    protected int barrierHeight;
+    protected int Distance;
+    public Course(int barrierHeight, int Distance){
+        this.barrierHeight = barrierHeight;
+        this.Distance = Distance;
+    }
+
+}
---
+++ src/HomeWork1/Main1.java
@@ -0,0 +1,16 @@
+package HomeWork1;
+
+public class Main1 {
+    public static void main(String[] args){
+
+        Team[] tM = {new Team("SouthPark","Kyle",1.2,900),
+                new Team("SouthPark","Butters",1.1,1000),
+                new Team("SouthPark","Stan",1.4,1200),
+                new Team("SouthPark","CartMan",0.5,400),
+                new Team("SouthPark","Kenny",2.1,2000)};
+        System.out.println("В забеге учавствуют " + tM[0].nameTeamMate + " " + tM[1].nameTeamMate + " " + tM[2].nameTeamMate + " " + tM[3].nameTeamMate+ " " + tM[4].nameTeamMate);
+
+        tM[0].race();tM[1].race();tM[2].race();tM[3].race();tM[4].killKenny();
+    }
+
+}
